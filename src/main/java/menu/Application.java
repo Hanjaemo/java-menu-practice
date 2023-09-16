a3144fccac895353ae5cce3b695bcec4144c9ec0
Hanjaemo <hjm2530@gmail.com>
2023-09-17 01:36:13 +0900

    feat(Application): 애플리케이션 시작 로직 구현

--- src/main/java/menu/Application.java
+++ src/main/java/menu/Application.java
@@ -1,7 +1,19 @@
 package menu;
 
+import menu.controller.RecommendController;
+import menu.domain.RandomNumberGenerator;
+import menu.domain.RecommendService;
+import menu.view.InputView;
+import menu.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        RecommendController controller = new RecommendController(
+                new MenuConfig(),
+                new RecommendService(new RandomNumberGenerator()),
+                new InputView(),
+                new OutputView()
+        );
+        controller.start();
     }
 }
